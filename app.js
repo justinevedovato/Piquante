d261a9535bc7a20bc738cd5fd111c90285ec8650
Justine Vedovato <justinevedovato@gmail.com>
2020-11-09 11:48:21 +0100

    création du serveur, du schema 'sauce' et test des premières routes

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Sauce = require("./models/sauce");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://justinev:uyRdLe0WUs1mRA9p@cluster0.3z3vl.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+// Enregistrer une nouvelle sauce
+app.post("api/sauces", (req, res, next) => {
+  delete req.body._id;
+  const sauce = new Sauce({
+    ...req.body,
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré!" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Modifier une sauce
+app.put("api/sauces/:id", (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié!" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Supprimer une sauce
+app.delete("/api/sauces/:id", (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Renvoie la sauce avec l'ID fourni
+app.get("/api/sauces/:id", (req, res, next) => {
+  Sauce.findById(req.params.id)
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Renvoie le tableau de toutes les sauces dans la base de données
+app.get("/api/sauces", (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+module.exports = app;
